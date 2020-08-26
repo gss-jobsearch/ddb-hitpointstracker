152fc5689395a4dcff75b062eb539f3cacc05f42
D&D Beyond <jobsearch+github@anthropohedron.net>
2020-08-26 16:30:01 -0400

    configured swagger and initialized the DB

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using HitPointsTracker.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace HitPointsTracker
 {
@@ -25,6 +21,14 @@ namespace HitPointsTracker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CharacterContext>(options =>
+            {
+                options.UseSqlite("Filename=characters.sqlite");
+            });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CharacterHP", Version = "v1" });
+            });
             services.AddControllers();
         }
 
@@ -36,11 +40,24 @@ namespace HitPointsTracker
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var db = scope
+                    .ServiceProvider
+                    .GetRequiredService<CharacterContext>();
+                db.Database.EnsureCreated();
+            }
 
+            //app.UseHttpsRedirection();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CharacterHP V1");
+                c.RoutePrefix = string.Empty;
+            });
             app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
