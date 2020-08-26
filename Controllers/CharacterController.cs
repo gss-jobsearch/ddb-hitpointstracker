caaea27676d9843eff4cc5cb1cdfa69d53e95341
D&D Beyond <jobsearch+github@anthropohedron.net>
2020-08-26 18:15:12 -0400

    implemented other endpoints

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -75,28 +75,95 @@ namespace HitPointsTracker.Controllers
         [HttpGet("{id}/current")]
         public async Task<IActionResult> GetHitPoints(long id)
         {
-            return Ok(0); //FIXME
+            Character? character = await _db.Characters
+                .FirstOrDefaultAsync(c => c.Id == id);
+            if (character == null) return NotFound();
+            return Ok(character.CurHitPoints + character.TempHitPoints);
         }
 
         [HttpPut("{id}/damage")]
         public async Task<IActionResult> Damage(long id,
             [FromQuery] string type, [FromQuery] int amount)
         {
-            return Ok(0); //FIXME
+            if (amount < 1)
+            {
+                return BadRequest("Damage must be greater than zero");
+            }
+            Character? character = await _db.Characters
+                .Include(c => c.Defenses)
+                .FirstOrDefaultAsync(c => c.Id == id);
+            if (character == null) return NotFound();
+            Defense? defense = character.Defenses
+                .FirstOrDefault(d => d.DamageType == type);
+
+            if (defense != null)
+            {
+                amount = defense.IsImmune ?
+                    0 : (amount / 2);
+            }
+            if (amount > 0)
+            {
+                if (character.TempHitPoints < amount)
+                {
+                    amount -= character.TempHitPoints;
+                    character.TempHitPoints = 0;
+                    character.CurHitPoints = (character.CurHitPoints < amount) ?
+                        0 : (character.CurHitPoints - amount);
+                }
+                else
+                {
+                    character.TempHitPoints -= amount;
+                }
+                _db.Characters.Update(character);
+                await _db.SaveChangesAsync();
+            }
+            return Ok(character.CurHitPoints + character.TempHitPoints);
         }
 
         [HttpPut("{id}/heal")]
         public async Task<IActionResult> Heal(long id,
             [FromQuery] int amount)
         {
-            return Ok(0); //FIXME
+            if (amount < 1)
+            {
+                return BadRequest("Amount healed must be greater than zero");
+            }
+            Character? character = await _db.Characters
+                .FirstOrDefaultAsync(c => c.Id == id);
+            if (character == null) return NotFound();
+
+            if (character.CurHitPoints < character.MaxHitPoints)
+            {
+                character.CurHitPoints += amount;
+                if (character.CurHitPoints > character.MaxHitPoints)
+                {
+                    character.CurHitPoints = character.MaxHitPoints;
+                }
+                _db.Characters.Update(character);
+                await _db.SaveChangesAsync();
+            }
+            return Ok(character.CurHitPoints + character.TempHitPoints);
         }
 
         [HttpPut("{id}/temp")]
         public async Task<IActionResult> AddTemp(long id,
             [FromQuery] int amount)
         {
-            return Ok(0); //FIXME
+            if (amount < 1)
+            {
+                return BadRequest("Temporary hit points added must be greater than zero");
+            }
+            Character? character = await _db.Characters
+                .FirstOrDefaultAsync(c => c.Id == id);
+            if (character == null) return NotFound();
+
+            if (amount > character.TempHitPoints)
+            {
+                character.TempHitPoints = amount;
+                _db.Characters.Update(character);
+                await _db.SaveChangesAsync();
+            }
+            return Ok(character.CurHitPoints + character.TempHitPoints);
         }
     }
 }
