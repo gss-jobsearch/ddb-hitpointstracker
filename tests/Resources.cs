92321146461282422695eee27973c328b48989bc
D&D Beyond <jobsearch+github@anthropohedron.net>
2020-08-27 15:48:20 -0400

    first unit test

---
+++ tests/Resources.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace HitPointsTracker.Tests
+{
+    public static class Resources
+    {
+        private static readonly Assembly TestAssembly =
+            typeof(Resources).Assembly;
+
+        public static Stream? GetResourceStream(
+            string filename)
+        {
+            var path = TestAssembly.FullName!.Split(',')
+               .Take(1)
+               .Concat(filename.Split('/', '\\'));
+
+            return TestAssembly.GetManifestResourceStream(
+                string.Join('.', path));
+        }
+
+        public static string? GetResourceString(
+            string filename,
+            Encoding? encoding = null)
+        {
+            using Stream? stream = GetResourceStream(filename);
+            if (stream == null) return null;
+            using TextReader reader = new StreamReader(stream,
+                encoding ?? Encoding.UTF8);
+            return reader.ReadToEnd();
+        }
+    }
+}
---
+++ tests/TestCharacterConversion.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using HitPointsTracker.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+using static HitPointsTracker.Tests.Resources;
+
+namespace HitPointsTracker.Tests
+{
+    [TestClass]
+    public class TestCharacterConversion
+    {
+        [TestMethod]
+        public void TConvertCharacter()
+        {
+            string json = GetResourceString("Data/briv.json")!;
+            var fullCharacter = JsonConvert.DeserializeObject<FullCharacter>(json);
+            var result = new List<ValidationResult>();
+            bool isValid = Validator.TryValidateObject(fullCharacter, new ValidationContext(fullCharacter), result);
+
+            Assert.IsTrue(isValid, "validation failed");
+
+            var character = fullCharacter.ToCharacter();
+
+            Assert.AreEqual(fullCharacter.Name, character.Name,
+                "name should have been copied");
+            Assert.AreEqual(41, character.MaxHitPoints,
+                "hit point calculation should match");
+        }
+
+    }
+
+}
