6d86bbba654df693bb95f074500a8623b1d52db3
D&D Beyond <jobsearch+github@anthropohedron.net>
2020-08-27 23:31:09 -0400

    test model validation

--- tests/TestCharacterConversion.cs
+++ tests/TestCharacterConversion.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using HitPointsTracker.Models;
@@ -12,15 +13,119 @@ namespace HitPointsTracker.Tests
     public class TestCharacterConversion
     {
         [TestMethod]
+        public void TCharacterValidation()
+        {
+            string json = GetResourceString("Data/briv.json")!;
+            var fullCharacter = JsonConvert.DeserializeObject<FullCharacter>(json);
+
+            MutateAndValidate(fullCharacter,
+                fc => 0,
+                fc =>
+                {
+                    fc.Classes![0].HitDiceValue = 7;
+                    return 1;
+                },
+                fc =>
+                {
+                    fc.Stats!.Strength = -1;
+                    return 1;
+                },
+                fc =>
+                {
+                    fc.Classes![0].ClassLevel -= 1;
+                    return 1;
+                },
+                fc =>
+                {
+                    fc.Level -= 1;
+                    return -1;
+                },
+                fc =>
+                {
+                    fc.Items![0].Modifier!.AffectedObject = null;
+                    return 1;
+                },
+                fc =>
+                {
+                    fc.Items![0].Modifier!.AffectedValue = null;
+                    return 1;
+                },
+                fc =>
+                {
+                    fc.Items![0].Modifier = null;
+                    return -2;
+                },
+                fc =>
+                {
+                    fc.Defenses![0].Defense = "weak";
+                    return 1;
+                },
+                fc =>
+                {
+                    fc.Defenses![0].Defense = null;
+                    return 0;
+                },
+                fc =>
+                {
+                    fc.Defenses![0].Type = null;
+                    return 1;
+                },
+                fc =>
+                {
+                    fc.Defenses = null;
+                    return -2;
+                },
+                fc =>
+                {
+                    fc.Classes![0] = fc.Classes![1];
+                    return -1;
+                },
+                fc =>
+                {
+                    fc.Stats!.Charisma = 23;
+                    return 1;
+                },
+                fc =>
+                {
+                    fc.Stats!.Strength = fc.Stats!.Charisma = 18;
+                    return -2;
+                }
+            );
+        }
+
+        private static void MutateAndValidate(
+            FullCharacter fullCharacter, params Func<FullCharacter, int>[] mutate)
+        {
+            int errors = 0;
+            var result = new List<ValidationResult>();
+            var context = new ValidationContext(fullCharacter);
+            bool isValid;
+            int index = 0;
+
+            foreach (var func in mutate)
+            {
+                errors += func(fullCharacter);
+                isValid = Validator.TryValidateObject(fullCharacter, context, result, true);
+                if (errors > 0)
+                {
+                    Assert.IsFalse(isValid, $"({index}) validation should have failed");
+                }
+                else
+                {
+                    Assert.IsTrue(isValid, $"({index}) validation shouldn't have failed");
+                }
+                Assert.AreEqual(errors, result.Count,
+                    $"({index}) wrong number of validation results");
+                result.Clear();
+                ++index;
+            }
+        }
+
+        [TestMethod]
         public void TConvertCharacter()
         {
             string json = GetResourceString("Data/briv.json")!;
             var fullCharacter = JsonConvert.DeserializeObject<FullCharacter>(json);
-            var result = new List<ValidationResult>();
-            bool isValid = Validator.TryValidateObject(fullCharacter, new ValidationContext(fullCharacter), result);
-
-            Assert.IsTrue(isValid, "validation failed");
-
             var character = fullCharacter.ToCharacter();
 
             Assert.AreEqual(fullCharacter.Name, character.Name,
